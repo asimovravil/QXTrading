5ac7d22827e65489d36eebd4d1497f044acd041d
Ravil <star.toni@mail.ru>
2023-11-30 17:08:50 +0600

    Initial Commit

---
+++ QXTrading/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QXTrading
+//
+//  Created by Ravil on 30.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
