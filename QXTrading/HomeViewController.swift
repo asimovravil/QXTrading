d4e5928570c9b2fddf91e0c19125d91ba6e48817
Ravil <star.toni@mail.ru>
2023-12-02 14:33:01 +0600

    [QX] navigationBarSetup bottom order navbar

--- QXTrading/HomeViewController.swift
+++ QXTrading/HomeViewController.swift
@@ -23,6 +23,28 @@ class HomeViewController: UIViewController {
         let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         navigationItem.leftBarButtonItems = [UIBarButtonItem(customView: imageView), flexibleSpace]
         navigationItem.rightBarButtonItem = settingsButton
+        
+        navigationController?.navigationBar.shadowImage = UIImage()
+        
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.backgroundColor = .black
+        
+        let borderBottom = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 1))
+        borderBottom.backgroundColor = R.color.colorShadowBottomNavBar()
+        borderBottom.translatesAutoresizingMaskIntoConstraints = false
+        
+        navigationController?.navigationBar.layoutIfNeeded()
+        navigationController?.navigationBar.addSubview(borderBottom)
+        
+        NSLayoutConstraint.activate([
+            borderBottom.leadingAnchor.constraint(equalTo: navigationController!.navigationBar.leadingAnchor),
+            borderBottom.trailingAnchor.constraint(equalTo: navigationController!.navigationBar.trailingAnchor),
+            borderBottom.bottomAnchor.constraint(equalTo: navigationController!.navigationBar.bottomAnchor),
+            borderBottom.heightAnchor.constraint(equalToConstant: 1)
+        ])
+        
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.compactAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
     }
-
 }
